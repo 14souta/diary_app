4852151d0233f00aea57bf7f3bf617e544280eff
14souta <tagusota@gmail.com>
2022-07-14 22:11:53 +0900

    投稿機能の設定

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -3,8 +3,21 @@ class DiariesController < ApplicationController
   end
 
   def new
+    @diary = Diary.new
   end
 
   def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:image, :explanation)
   end
 end
