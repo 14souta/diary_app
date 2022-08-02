9c5b810e68445657a002dcc251fdaf72936ecaac
14souta <tagusota@gmail.com>
2022-08-02 12:06:32 +0900

    destroyアクションの追加

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,6 +1,6 @@
 class DiariesController < ApplicationController
 
-before_action :diary_date, only: [:show, :edit, :update]
+before_action :diary_date, only: [:show, :edit, :update, :destroy]
 
   def index
     @diaries = Diary.all
@@ -31,6 +31,11 @@ before_action :diary_date, only: [:show, :edit, :update]
   end
 
   def show
+  end
+
+  def destroy
+    @diary.destroy
+    redirect_to root_path
   end
 
   private
