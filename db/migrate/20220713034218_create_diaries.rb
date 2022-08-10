c36281eb468af9dfe777615d32db3b26121a4dab
14souta <tagusota@gmail.com>
2022-08-10 17:13:51 +0900

    diaryにuserを紐付け

--- db/migrate/20220713034218_create_diaries.rb
+++ db/migrate/20220713034218_create_diaries.rb
@@ -2,6 +2,7 @@ class CreateDiaries < ActiveRecord::Migration[6.0]
   def change
     create_table :diaries do |t|
       t.text :explanation, null: false
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(version: 2022_08_02_070110) do
 
   create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "explanation", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_diaries_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -53,4 +55,5 @@ ActiveRecord::Schema.define(version: 2022_08_02_070110) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "diaries", "users"
 end
